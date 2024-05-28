7bd20bdbf835866e8de844f8764ec2d1c082349c
Safepost2000 <safepost2000@gmail.com>
2024-05-29 02:20:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import csv
 import cv2
 import streamlit as st
 from paddleocr import PaddleOCR, draw_ocr
-from numpy import np
+from numpy
 def extract_invoice_info(image):
     # Run OCR
     ocr = PaddleOCR(use_gpu=False, lang="en")
